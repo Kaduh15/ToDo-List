7e847394edee00d7c9f8d5606c619f85014349d9
kadu <kadu.silva2014@gmail.br>
2022-09-21 17:26:25 -0300

    Feat:✳ BackEnd Dockerizado 🐋

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 require('dotenv').config();
 
-PORT = process.env.BACK_PORT || 3001;
+PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
