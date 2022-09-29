356278288ce9419a44fd484931b1a2dbba92e300
kadu <kadu.silva2014@gmail.br>
2022-09-29 20:07:53 -0300

    Feat:✳ encode the ip in the token

--- backend/src/routes/authRouter.js
+++ backend/src/routes/authRouter.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const { StatusCodes } = require('http-status-codes');
+const { generateHash } = require('../utils/hash');
 const { authTokenValidation } = require('../utils/JWT');
+const { throwError } = require('../utils/mapError');
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
   const { authorization } = req.headers;
+  const { user } = req;
+  const ipClient = req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
 
   if (!authorization) return res.status(StatusCodes.BAD_REQUEST).json({
     message: 'Token not existing'
   });
 
+  
   authTokenValidation(authorization);
+  
+  if (user.ip === generateHash(ipClient)) throwError({ message: 'Relogin', status: StatusCodes.UNAUTHORIZED });
 
   res.sendStatus(StatusCodes.OK);
 })
