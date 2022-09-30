8233f3a1bb6308d21a5bfbc5e52c3aa54452e92e
kadu <kadu.silva2014@gmail.br>
2022-09-30 10:14:28 -0300

    node heroku

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,9 +1,17 @@
 require('dotenv').config();
 
+console.log({
+  database: process.env.DATABASE,
+  dialect: 'postgres',
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASSWORD,
+  username: process.env.DB_USERNAME,
+});
+
 module.exports = {
   development: {
     database: process.env.DATABASE,
-    dialect: 'mysql',
+    dialect: process.env.DIALECT,
     host: process.env.DB_HOST,
     password: process.env.DB_PASSWORD,
     username: process.env.DB_USERNAME,
@@ -17,7 +25,7 @@ module.exports = {
   },
   production: {
     database: process.env.DATABASE,
-    dialect: 'postgresql',
+    dialect: 'postgres',
     host: process.env.DB_HOST,
     password: process.env.DB_PASSWORD,
     username: process.env.DB_USERNAME,
