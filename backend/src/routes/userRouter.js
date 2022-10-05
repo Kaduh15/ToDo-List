3957ece3a566e0e5370851fa6065fe87eb422bae
kadu <kadu.silva2014@gmail.br>
2022-10-05 13:14:20 -0300

    Refactor:♻ Enrollment route without token authentication

--- backend/src/routes/userRouter.js
+++ backend/src/routes/userRouter.js
@@ -6,6 +6,12 @@ const userCreateValidation = require('../middlewares/userCreateValidation');
 const { authAccess } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
+router.post('/', userCreateValidation, async (req, res) => {
+  const {type, data} = await userController.createUser(req.body);
+
+  res.status(map(type)).json(data);
+})
+
 router.use(authAccess);
 
 router.get('/', userController.getUser)
@@ -14,12 +20,6 @@ router.get('/:id', async (req, res) => {
   const { id } = req.params;
 
   const {type, data} = await userController.getById(id);
-
-  res.status(map(type)).json(data);
-})
-
-router.post('/', userCreateValidation, async (req, res) => {
-  const {type, data} = await userController.createUser(req.body);
 
   res.status(map(type)).json(data);
 })
